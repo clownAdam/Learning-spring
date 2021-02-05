6bae2dc114c3469f85e26bb43e9776fda01b60c0
zkr_chensiqi <3406869297@qq.com>
2021-02-05 16:30:25 +0800

    set injection

---
+++ power-node/src/main/java/cn/clown/domain/School.java
@@ -0,0 +1,36 @@
+package cn.clown.domain;
+
+/**
+ * <description>学校-实体类</description>
+ *
+ * @author clown
+ * @date 2021
+ */
+public class School {
+    private String name;
+    private String address;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
